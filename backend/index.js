9fed3f5a431c40bbef11c578c34ab382baa2f4a5
Ankur <ankur.rangi08@gmail.com>
2023-12-09 14:54:44 +0530

    Middleware for handling CORS POLICY

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,7 @@ const app = express();
 app.use(express.json());
 
 
-// Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
-// app.use(cors());
-// Option 2: Allow Custom Origins
+// Middleware for handling CORS POLICY, Allow "Custom" Origins
 app.use(
   cors({
     origin: 'http://localhost:3000',
