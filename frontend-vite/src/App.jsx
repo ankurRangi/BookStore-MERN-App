6461d98ea93d2c8d4e77574d99b2fa86af640b94
Ankur <ankur.rangi08@gmail.com>
2023-12-09 19:58:25 +0530

    Created CRUD apis for React

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import ShowBook from "./pages/ShowBook"
 import CreateBooks from "./pages/CreateBooks"
-import EditBooks from "./pages/EditBooks"
-import DeleteBooks from "./pages/DeleteBooks"
+import EditBook from "./pages/EditBook"
+import DeleteBook from "./pages/DeleteBook"
 
 
 const App = () => {
@@ -13,8 +13,8 @@ const App = () => {
       <Route path='/' element={<Home />} />
       <Route path='/books/create' element={<CreateBooks />} />
       <Route path='/books/details/:id' element={<ShowBook />} />
-      <Route path='/books/edit/:id' element={<EditBooks />} />
-      <Route path='/books/delete/:id' element={<DeleteBooks />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
       </Routes>
   );
 };
